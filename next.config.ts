255cf42aafb4b252e3f389560e05accc9f7c430b
PHUNG VAN DOAN <phungvandoan2k3@gmail.com>
2024-11-17 22:53:02 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    config.module.rules.push({ test: /\.node$/, use: "raw-loader" });
+
+    if (!isServer) config.externals.push("canvas");
+
+    return config;
+  },
 };
 
 export default nextConfig;
